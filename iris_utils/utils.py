ad9f0bdf8675bac3a7ee08d4579c14c970e463e3
erik.holmgren <a002512@c21725.ad.smhi.se>
2022-06-28 16:11:38 +0200

    New utilities: merge_aeq_cubes and attribute_to_aux
    - merge_aeq_cubes makes it possible to merge cubes that are almost equal, in that coordinates are the same but differ in small ways, i.e. significant digits. It also deals with different calendars.
    
    - attribute_to_aux takes any number of cube attributes and add these as a scalar coordinate.

--- iris_utils/utils.py
+++ iris_utils/utils.py
@@ -1,5 +1,7 @@
 from shapely.geometry import MultiPoint
 import numpy as np
+from iris.exceptions import MergeError
+import iris
 
 
 def mask_from_shape(cube, shape, coord_names=("latitude", "longitude")):
@@ -50,3 +52,92 @@ def mask_from_shape(cube, shape, coord_names=("latitude", "longitude")):
         mask = np.broadcast_to(mask, cube.shape)
 
     return mask
+
+
+def merge_aeq_cubes(cubes):
+    """Merge almost equal cubes.
+    Wrapper for CubesList.merge_cube() which first tries a normal merge_cube
+    and if unsuccessful potentially check whether the coordinates are close.
+    If the cubes are on different coordinate system a manual adjustment is needed.
+
+    Arguments
+    ---------
+    cubes : CubeList
+        List of cubes to merge.
+
+    """
+
+    # First we try and merge the cubes.
+    try:
+        cubes.merge_cube()
+
+    # If this doesn't work, we have to figure out what is wrong.
+    except MergeError:
+        # The first cube in the list is used a base.
+        cube0 = cubes[0]
+        cube0_lat_points = cube0.coord("grid_latitude").points
+        cube0_lon_points = cube0.coord("grid_longitude").points
+
+        # Loop over the rest of the cubes.
+        for i, cube in enumerate(cubes[1:]):
+            candidate_lat_points = cube.coord("grid_latitude").points
+            # Check the coordinate system against the base cube.
+            if cube.coord_system() != cube0.coord_system():
+                print(
+                    f"Coordinate system mismatch at cube {i+1}: "
+                    " {cube.coord_system()} vs. {cube0.coord_system()}"
+                )
+            # If coord system match it is likely a "precision" error.
+            # We check if the points array are not equal but they are close.
+            elif (np.all(np.isclose(cube0_lat_points, candidate_lat_points))) and not (
+                np.all(np.equal(cube0_lat_points, candidate_lat_points))
+            ):
+                print("Converting coordinates.")
+                # Set the points of the candidate to the points of the base cube.
+                cube.coord("grid_latitude").points = cube0_lat_points.copy()
+                cube.coord("grid_longitude").points = cube0_lon_points.copy()
+                # And bounds
+                cube.coord("grid_latitude").bounds = cube0.coord(
+                    "grid_latitude"
+                ).bounds.copy()
+                cube.coord("grid_longitude").bounds = cube0.coord(
+                    "grid_longitude"
+                ).bounds.copy()
+                # Also have to overwrite the aux coord.
+                cube.coord("latitude").points = cube0.coord("latitude").points.copy()
+                cube.coord("longitude").points = cube0.coord("longitude").points.copy()
+                # And bounds
+                cube.coord("latitude").bounds = cube0.coord("latitude").bounds.copy()
+                cube.coord("longitude").bounds = cube0.coord("longitude").bounds.copy()
+            # Calendar might be different as well.
+            if not cube0.coord("time").units == cube.coord("time").units:
+                print("Converting calendar.")
+                # Convert the calendar of the candidate cube to the base cube.
+                cube.coord("time").convert_units(cube0.coord("time").units)
+
+        # With this done we try to merge the list again.
+        return cubes.merge_cube()
+
+
+def attribute_to_aux(cubes, attribute_names=["driving_model_id", "model_id"]):
+    """Add an attribute from the cube as a scalar coordinate.
+
+    Arguments
+    ---------
+    cubes : iris.CubeList
+        List of cubes to perform the operation on.
+    attribute_names : list(string, string)
+        List of names of the attributes to use for the new coordinate.
+    """
+
+    # Loop over all the cubes.
+    for cube in cubes:
+        # Create a new coordinate from the attribute.
+        coord = (
+            cube.attributes[attribute_names[0]]
+            + "--"
+            + cube.attributes[attribute_names[1]]
+        )
+        new_aux_coord = iris.coords.AuxCoord(coord, var_name="model_conf")
+        # Add it to the cube.
+        cube.add_aux_coord(new_aux_coord)
